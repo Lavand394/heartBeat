a5e4fa89b85753d9f115cf3b49bf007a95e64e7c
Paulina Munoz <pmunoz.lien@outlook.com>
2020-04-22 19:00:03 -0500

    001- Project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {ConnectionService} from 'ng-connection-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heartBeat';
+  title = 'hearbeat';
+   status = 'ONLINE';
+   isConnected = true;
+
+   constructor(private ConnectionService: ConnectionService){
+     this.ConnectionService.monitor().subscribe(isConnected =>  {
+       this.isConnected = isConnected;
+       if(this.isConnected){
+         this.status = "ONLINE";
+       } else {
+         this.status = "OFFLINE"
+       }
+       alert(this.status);
+     });
+   }
 }
